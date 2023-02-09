a360b86f2723d02dfbf750d22057437f1db4dce9
Keith Charles <kcharles1972@gmail.com>
2023-02-09 00:19:19 -0700

    answer in lua

--- 507 perfect number/507_perfect_number.lua
+++ 507 perfect number/507_perfect_number.lua
@@ -1,19 +1,14 @@
---[[
-  https://leetcode.com/problems/perfect-number/
+local check_perfect_number = function (num)
+  local i = 2
+  local total = 1
 
-  A perfect number is a positive integer that is equal to the sum of its positive divisors,
-  excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+  while i * i < num do
+    if num % i == 0 then total = total + i + num/i end
+    i = i + 1;
+  end
 
-  Given an integer n, return true if n is a perfect number, otherwise return false.
+  return total == num
+end
 
-  Example 1:
-  Input: num = 28
-  Output: true
-  Explanation: 28 = 1 + 2 + 4 + 7 + 14
-  1, 2, 4, 7, and 14 are all divisors of 28.
-  
-  Example 2:
-  Input: num = 7
-  Output: false
-]]--
-
+assert(check_perfect_number(28))
+assert(not check_perfect_number(7))
